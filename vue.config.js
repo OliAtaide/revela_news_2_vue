6f434f05de9a4235367216cad036d08695d554ce
Filipe de Oliveira de Ataíde <filipe.ataide@lais.huol.ufrn.br>
2024-04-29 11:07:13 -0300

    test

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/revela_news_2_vue_dist/'
-    : '/'
 });
